fdf0de5c4367aed37a5cf33eafcd57f60db0e673
fernandochristyanto <christyantofernando@gmail.com>
2018-01-15 00:56:24 +0700

    Add reveal-on-scroll feature to features section

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,13 +1,30 @@
 import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll{
     constructor(){
         this.itemsToReveal = $(".feature-item");
         this.hideInitially();
+        this.createWaypoints();
     }
 
     hideInitially(){
         this.itemsToReveal.addClass("reveal-item");
+        this.itemsToReveal.addClass("reveal-item--left-offset");
+    }
+
+    createWaypoints(){
+        this.itemsToReveal.each(function(){
+            var currentItem = this;
+            new Waypoint({
+                element : currentItem,
+                handler : function(){
+                    $(currentItem).addClass("reveal-item--is-visible");
+                    $(currentItem).addClass("reveal-item--no-offset");
+                },
+                offset : "85%"
+            });
+        });
     }
 }
 
